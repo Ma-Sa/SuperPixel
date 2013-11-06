9c2441d3d327c1fb17d4a15c3aa29a70791ef7b7
Ma-Sa <masa@gwmail.gwu.edu>
2013-11-05 19:28:29 -0500

    modification

---
+++ src/Point.cpp
@@ -0,0 +1,15 @@
+#include "Point.h"
+
+namespace superpixel{
+
+Point::point Point::Create3dPoint(cv::Mat depth, int i, int j ){
+   Point::point point3D;
+   point3D.pd = depth.at<unsigned short>(i,j);
+   point3D.px = i;
+   point3D.py = j;
+   //point3D.position = new Eigen::Vector3f((float)point3D.px,(float)point3D.py, point3D.pd);
+   return point3D;
+}
+
+}
+
